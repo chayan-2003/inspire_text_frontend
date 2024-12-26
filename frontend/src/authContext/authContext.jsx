40bf81c9f64bac96615ce624bab501ed1a01dae3
Your Name <your.email@example.com>
2024-12-27 02:35:55 +0530

    some_upd

--- frontend/src/authContext/authContext.jsx
+++ frontend/src/authContext/authContext.jsx
@@ -9,11 +9,12 @@ export const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false); // Authentication state
   const [loading, setLoading] = useState(true); // Loading state for auth check
   const API_URL = import.meta.env.PROD ? import.meta.env.VITE_API_URL_PROD : import.meta.env.VITE_API_URL_DEV;
+
   // Check authentication status
   useEffect(() => {
     const checkAuthStatus = async () => {
       try {
-        const response = await axios.get( `${API_URL}/api/users/auth/check`, {
+        const response = await axios.get(`${API_URL}/api/users/auth/check`, {
           withCredentials: true, // Include cookies in the request
         });
         if (response.status === 200) {
@@ -32,8 +33,12 @@ export const AuthProvider = ({ children }) => {
     checkAuthStatus();
   }, []);
 
+  const logout = () => {
+    setIsAuthenticated(false);
+  };
+
   return (
-    <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
+    <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated, logout }}>
       {loading ? <div>Loading...</div> : children}
     </AuthContext.Provider>
   );
